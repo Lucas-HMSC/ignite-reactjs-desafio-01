f7348c3c8ddd595d32431183ddb43ec4453b9593
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-12-09 21:49:41 -0300

    Desafio Concluído

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle.trim()) {
+      setTasks([...tasks, {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false,
+      }]);
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map((task) => 
+      task.id === id ? {
+        ...task,
+        isComplete: !task.isComplete
+      } : task);
+    
+    setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter((item) => item.id !== id);
+    setTasks(newTasks);
   }
 
   return (
